5139738d55d9ab46f0a7ebf672250f92e4bb0fd5
Jefin Rojar <jefinrojar@gmail.com>
2024-05-28 21:19:14 +0530

    first commit

---
+++ jobs.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- header starts -->
+    <nav>
+        <a href="home.php">Home</a>
+        <a href="jobs.php">Jobs</a>
+        <a href="about.php">About us</a>
+        <a href="contact.php">Contact us</a>
+    </nav>
+    <!-- header ends -->
+    <h1>jobs</h1>
+</body>
+</html>
